dfe62ad4e2e3439dedc74c82dd87e4e07be5ebf5
PranitPatil03 <patilpranit3112@gmail.com>
2023-01-02 23:33:57 +0530

    Home Page Completed

--- classforma/src/Components/Home.js
+++ classforma/src/Components/Home.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import '../Components/style.css';
 
 export default function Home() {
   return (
     <>
-    Home
+      <div className="heading">
+        <h2 className="headline">Documents</h2>
+      </div>
+      <div className="line"></div>
+      <div className="links">
+        <div className="link1">
+          - <a href="https://arxiv.org/pdf/2212.08011.pdf">Sample Document 1</a>
+        </div>
+        <div className="link2">
+          - <a href="https://arxiv.org/pdf/2212.07937.pdf">Sample Document 2</a>
+        </div>
+        <div className="link3">
+          - <a href="https://arxiv.org/pdf/2212.07931.pdf">Sample Document 3</a>
+        </div>
+      </div>
     </>
-)
+  )
 }
