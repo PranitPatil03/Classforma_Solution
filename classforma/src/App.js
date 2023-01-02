bd2402800543fc8745fe948d0f7b88e8478cc021
PranitPatil03 <patilpranit3112@gmail.com>
2023-01-03 01:00:58 +0530

    Main Page Frontend Started

--- classforma/src/App.js
+++ classforma/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Home from './Components/Home';
+// import Home from './Components/Home';
+import Main from './Components/Main';
 
 function App() {
   return (
     <>
-    <Home/>
+    {/* <Home/> */}
+    <Main/>
     </>
   );
 }
--- classforma/src/Components/Main.js
+++ classforma/src/Components/Main.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import '../Components/style.css';
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <>
+      <div className="content">
+        <div className="heading">
+          <h2 className="headline">Labels</h2>
+        </div>
+        <div className="line"></div>
+
+        <div className="buttons">
+          <div className="titlebtn">
+            <button className="title">Title</button>
+          </div>
+          <div className="authorbtn">
+            <button className="author">Author</button>
+          </div>
+        </div>
+
+        <div className="heading boxes">
+          <h2 className="headline">Boxes</h2>
+        </div>
+        <div className="line"></div>
+      </div>
+
+      <div className="pdf">
+
+      </div>
+
+    </>
   )
 }
